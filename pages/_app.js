02e6996381e4b6d43cac8d54387bf5404b0dd20f
Tadeosoto <tadeosoto1993@gmail.com>
2024-03-20 13:09:57 -0600

    Transition
    se hizo una transition de 3 capas (posiblemente le cambie el color luego)

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,25 @@
 import "../styles/globals.css";
+import { motion, AnimatePresence } from "framer-motion";
 
 // componentes aqui
 
 import Layout from "../components/Layout";
+import Transition from "../components/Transition";
+
+// Router
+
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   return (
     <Layout>
-      <Component {...pageProps} />
+      <AnimatePresence mode="wait">
+        <motion.div key={router.route} className="h-full">
+          <Transition />
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
     </Layout>
   );
 }
