b910f703071e898b9b05e59937ba8a2223e82f16
Tadeosoto <tadeosoto1993@gmail.com>
2024-03-21 23:51:57 -0600

    se agrego foto abvout

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 // icons
 import {
   FaHtml5,
@@ -87,8 +89,57 @@ const aboutData = [
   },
 ];
 
+//  Componentes
+
+import Avatar from "../../components/Avatar";
+import Circles from "../../components/Circles";
+
+// framer
+
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
+
 const About = () => {
-  return <div>About</div>;
+  const [index, setIndex] = useState(0);
+  console.log(index);
+  return (
+    <div className="h-full bg-primary/30 py-32 text-center xl:text-left">
+      <Circles />
+      {/* avatar */}
+      <motion.div
+        variants={fadeIn("right", 0.2)}
+        initial="hidden"
+        animate="show"
+        exit="hidden"
+        className="hidden xl:flex absolute bottom-0 -left-[370px]"
+      >
+        <Avatar />
+      </motion.div>
+      <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
+        <div>text</div>
+        <div>
+          <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
+            {aboutData.map((item, itemIndex) => {
+              return (
+                <div
+                  key={itemIndex}
+                  className={`${
+                    index === itemIndex &&
+                    "text-blue-600 after:w-[100%] after:bg-blue-600 after:transition-all after:duration-300"
+                  } cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
+                  onClick={() => setIndex(itemIndex)}
+                >
+                  {item.title}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
+
+// NOS QUEDAMOS EN EL SEGUNDO 1:21:50 DEL VIDEO
