5864877492b42d02c2c0f14420cb448195272f65
Tadeosoto <tadeosoto1993@gmail.com>
2024-03-26 01:54:09 -0600

    se completo la seccion 'work'
    falta arreglar como se ven las imagenes en pantalla completa

--- pages/services/index.js
+++ pages/services/index.js
@@ -26,7 +26,7 @@ const Services = () => {
               exit="hidden"
               className="h2 xl:mt-8"
             >
-              Mis servicios <span className="text-accent">.</span>
+              Mis servicios<span className="text-accent">.</span>
             </motion.h2>
             <motion.p
               variants={fadeIn("up", 0.4)}
